ba2d576e3ba0ed2487c9716b343169eed1b12213
davideblaffard <infodavide98@gmail.com>
2024-12-04 13:25:32 +0100

    add: added CalcoliHelper class

---
+++ java.calcolatrice/src/org/lessons/java/utils/App.java
@@ -0,0 +1,6 @@
+package org.lessons.java.utils;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ java.calcolatrice/src/org/lessons/java/utils/CalcoliHelper.java
@@ -0,0 +1,17 @@
+package org.lessons.java.utils;
+
+public class CalcoliHelper {
+    private CalcoliHelper() {
+        
+    }
+
+    // Metodo per sommare due numeri interi
+    public static int somma(int primoValore, int secondoValore) {
+        return primoValore + secondoValore;
+    }
+
+    // Metodo per sommare due numeri double
+    public static double somma(double primoValore, double secondoValore) {
+        return primoValore + secondoValore;
+    }
+}
